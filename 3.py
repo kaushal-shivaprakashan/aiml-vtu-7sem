9232172c9182df9f01dcfb02f6ce19b6df1de653
Kaushal Shivaprakash <86502925+kaushal-shivaprakashan@users.noreply.github.com>
2022-12-27 22:14:31 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -64,3 +64,13 @@ Final Specific hypothesis:
 
 Final General hypothesis:
  [['Sunny', '?', '?', '?', '?', '?'], ['?', 'Warm', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]
+
+
+
+
+
+###################################################csv#############################
+Sunny,Warm,Normal,Strong,Warm,Same,Yes
+Sunny,Warm,High,Strong,Warm,Same,Yes
+Rainy,Cold,High,Strong,Warm,Change,No
+Sunny,Warm,High,Strong,Cool,Change,Yes
